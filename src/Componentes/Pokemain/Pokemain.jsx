2ca2b68da7250c7eac2c53347c7012dc7a5e6fc3
ArteagaJuan <dearteagas4@gmail.com>
2022-09-29 17:57:54 -0300

    desarrolandoPokeInput

--- src/Componentes/Pokemain/Pokemain.jsx
+++ src/Componentes/Pokemain/Pokemain.jsx
@@ -1,30 +1,59 @@
-import "./Pokemain.css"
+import "./Pokemain.css";
 import { Link } from "react-router-dom";
 
 const Pokemain = ({ pokemones, valorId }) => {
-    // const selectPokemon = ({pokemonId}) => {
-    //     valorId(pokemonId)
-    // }
-    return (
-        <div className="pokemain-global">
-            {pokemones.map((pokemon) => {
-                return (
-                    <Link className="link" to= {`/Pokecard/${pokemon.name}`}>
-                        <div key={parseInt(pokemon.id)} style={{ borderColor: pokemon.bckcolor }} className="pokemain-container">
-                            <p className="pokemain-id" style={{ color: pokemon.bckcolor }}># {pokemon.id}</p>
-                            <div className="pokemain-container-img"><img src={pokemon.img} className="pokemain-img" /></div>
-                            <p className="pokemain-name" style={{ backgroundColor: pokemon.bckcolor }}>{pokemon.name}</p>
-                        </div>
-                    </Link>
-                )
-            })}
+  // const selectPokemon = ({pokemonId}) => {
+  //     valorId(pokemonId)
+  // }
+  return (
+    <div className="pokemain-global">
+      <Link className="link" to={`/Pokecard/PokeInput`}>
+        <div
+          style={{ borderColor: "black" }}
+          className="pokemain-container"
+        >
+          <p className="pokemain-id" style={{ color: "black" }}>
+            ?
+          </p>
+          <div className="pokemain-container-img">
+            <img src="../img/signo_suma.jpg" className="pokemain-img" />
+          </div>
+          <p
+            className="pokemain-name"
+            style={{ backgroundColor: "black"}}
+          >
+          Agregar Pokemon
+          </p>
         </div>
-    );
-}
+      </Link>
+      {pokemones.map((pokemon) => {
+        return (
+          <Link className="link" to={`/Pokecard/${pokemon.name}`}>
+            <div
+              key={parseInt(pokemon.id)}
+              style={{ borderColor: pokemon.bckcolor }}
+              className="pokemain-container"
+            >
+              <p className="pokemain-id" style={{ color: pokemon.bckcolor }}>
+                # {pokemon.id}
+              </p>
+              <div className="pokemain-container-img">
+                <img src={pokemon.img} className="pokemain-img" />
+              </div>
+              <p
+                className="pokemain-name"
+                style={{ backgroundColor: pokemon.bckcolor }}
+              >
+                {pokemon.name}
+              </p>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Pokemain;
 
 // onClick={()=>selectPokemon(pokemon.id)}
-
-
-
--- src/Componentes/RoutesApp/RoutesApp.jsx
+++ src/Componentes/RoutesApp/RoutesApp.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Pokecard from "../Pokecard/Pokecard";
 import data from "../../Data/data";
-import App from "../../App"
-
+import App from "../../App";
+import PokeInput from "../PokeInput/PokeInput";
 const RoutesApp = () => {
     return (
         <>
           <Routes>
             <Route path="/" element={<App/>}/>
             <Route path="/Pokecard/:nombre" element={< Pokecard pokemones={data} />}/>
+            <Route path="/Pokecard/PokeInput" element={< PokeInput/>}/>
           </Routes>
         </>
     );
